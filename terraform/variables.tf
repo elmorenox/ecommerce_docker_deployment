bfeb55b23276b9bcda56849ce5a146987603e929
luis <morenodoesinfra@gmail.com>
2024-10-30 18:04:21 +0000

    tf variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,20 +1,18 @@
 variable "db_name" {
   description = "Database name"
   type        = string
+  default = "e-commerce"
 }
 
 variable "db_username" {
   description = "Database username"
   type        = string
+  default = "user"
 }
 
 variable "db_password" {
   description = "Database password"
   type        = string
   sensitive   = true
-}
-
-variable "key_name" {
-  description = "Name of the EC2 key pair"
-  type        = string
+  default = "password"
 }
