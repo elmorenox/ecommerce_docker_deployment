03d45729f84547e5996ff60a5108715254cf567b
luis <morenodoesinfra@gmail.com>
2024-10-31 05:47:40 +0000

    Adding alb to allowed host

--- backend/my_project/settings.py
+++ backend/my_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-*7!!kc@bmtx8ngui6lr@xmifmcwm6y%hnbe)rdei(b!ds8t)uq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["0.0.0.0"] # add the private ip of where you've placed this Django app
+ALLOWED_HOSTS = ["ecommerce-alb-1473803045.us-east-1.elb.amazonaws.com"] # add the private ip of where you've placed this Django app
 
 # Application definition
 
