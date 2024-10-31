0e6bf780baa3453439db7de265e0b086a34415da
luis <morenodoesinfra@gmail.com>
2024-10-31 00:30:45 +0000

    oops

--- terraform/main.tf
+++ terraform/main.tf
@@ -300,7 +300,7 @@ resource "aws_security_group" "bastion" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0g"]  # Replace with your public IP address
+    cidr_blocks = ["0.0.0.0/0"]  # Replace with your public IP address
   }
 
   egress {
