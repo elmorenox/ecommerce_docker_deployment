450c263eda88a6db40ad244a47d19b7cfb7862c9
luis <morenodoesinfra@gmail.com>
2025-01-22 04:27:15 +0000

    allow bastion to reach rds db

--- terraform/main.tf
+++ terraform/main.tf
@@ -119,7 +119,6 @@ resource "aws_security_group" "rds" {
     from_port       = 5432
     to_port         = 5432
     protocol        = "tcp"
-    security_groups = [aws_security_group.ec2.id]
   }
 
   ingress {
